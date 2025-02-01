519dec0e29cfb8f80f31dc4d299c1c00f5f59774
LoganFireofall <joshden1216@gmail.com>
2025-01-31 22:03:08 -0500

    I Really don't know what I am doing.

---
+++ source/java/GUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GUI {
+    private JButton button1;
+    private JPanel panel1;
+    private JButton simpleButton;
+    private JButton button3;
+    private JButton simonButton;
+    private JButton button5;
+    private JButton timingButton;
+    private JButton authenticatorButton;
+    private JButton customizeSoundsButton;
+}
