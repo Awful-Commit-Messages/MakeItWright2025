438f08ca1916e305fa29037d15d9f0293df71d7a
honeypint <122322065+honeypint@users.noreply.github.com>
2025-02-01 00:58:46 -0500

    (mostly) utilize json parsing to store user data between runs

--- source/java/User.java
+++ source/java/User.java
@@ -1,7 +1,8 @@
+import com.google.gson.*;
 public class User {
-    String username; // user's username
-    String salt; // salt dedicated to this user
-    String hash; // Hashed password
+    private String username; // user's username
+    private String salt; // salt dedicated to this user
+    private String hash; // Hashed password
 
     // Default case
     public User() {
@@ -17,6 +18,22 @@ public class User {
         this.hash = hash;
     }
 
+    // Getters for a couple of varaibles
     public String getSalt() { return salt; }
     public String getUsername() { return username; }
+    public String getHash() { return hash; }
+
+    // serialize: Serialize this user into a JsonObject.
+    JsonObject serialize() {
+        JsonObject json = new JsonObject();
+		json.addProperty("username", username);
+		json.addProperty("salt", salt);
+        json.addProperty("hash", hash);
+		return json;
+    }
+
+    // deserialize: Deserialize this user from a JsonObject.
+    static User deserialize(JsonObject json) {
+        return new User(json.get("username").getAsString(), json.get("salt").getAsString(), json.get("hash").getAsString());
+    }
 }
